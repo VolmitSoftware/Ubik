200bd92171ddfc17e3307007768d1667e5e535ac
Brian Neumann-Fopiano <mypsn1550@gmail.com>
2023-03-23 09:34:26 -0400

    Base Project

---
+++ src/main/java/com/volmit/ubik/Ubik.java
@@ -0,0 +1,17 @@
+package com.volmit.ubik;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Ubik extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
